e61d1c29b36220c4434b0499688b1d0445933758
Utkarsh Tripathi <utarts.in@gmail.com>
2026-01-06 04:16:53 +0530

    New update

--- includes/meta.php
+++ includes/meta.php
@@ -62,4 +62,4 @@ $currentUrl = "https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" rel="stylesheet" />
-<link rel="stylesheet" href="styles.css">
+<link rel="stylesheet" href="styles.css?v=2.0">
